66dfcea06b793b8b0df0dc512c050f6141b6e3ab
Sourjyabrata Dey <sourjya.dey2004@gmail.com>
2025-10-27 22:34:49 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('/.../best.pt')
+    model = YOLO(MODEL)
     '''model.train(
                 data='Dataset/data.yaml',
                 epochs=100,
@@ -12,3 +12,4 @@ if __name__ == '__main__':
     
 
     model.predict('potholes.mp4', show=True)
+
